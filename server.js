e248639f85c6a78c2482b89b9f7fed981e54f60f
surendra <surendra@otpl.com.au>
2020-09-06 03:03:54 +0530

    Azure setting final build V1.3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(function (request, response) {
   response.end(message)
 })
 
-const port = process.env.PORT || 1337
+const port = process.env.PORT || 8080
 server.listen(port)
 
 console.log('Server running at port', port)
